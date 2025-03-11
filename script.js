b0b82132d12aa54830c784d9dd23f3a07a9c878a
wesleysaraivaa <wesley-saraiva@outlok.com>
2025-03-10 22:20:02 -0300

    (FEAT: script.js): aff funcao de adicionar itens no carrinho

--- script.js
+++ script.js
@@ -1,25 +1,95 @@
 const menu = document.getElementById("menu");
-const cartBtn = document.getElementById("cart-btn");
+const cartbtn = document.getElementById("cart-btn");
 const cartModal = document.getElementById("cart-modal");
 const cartItemsContainer = document.getElementById("cart-itens");
 const cartTotal = document.getElementById("cart-total");
-const checkoutBTN = document.getElementById("checkout-btn");
+const checkoutBtn = document.getElementById("checkout-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
-const cartCounter = document.getElementById("cart-count");
-const addressinput = document.getElementById("address");
+const cartCount = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
-//abrir modal do carrinho
-cartBtn.addEventListener("click", function () {
+let cart = [];
+
+cartbtn.addEventListener("click", function () {
   cartModal.style.display = "flex";
 });
 
-//fechar modal do carrinho
-cartBtn.addEventListener("click", function (event) {
-  if (event.target == cartModal) {
-    cartModal.style.display = "none";
-  }
-});
 closeModalBtn.addEventListener("click", function () {
   cartModal.style.display = "none";
 });
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+});
+
+menu.addEventListener("click", function (event) {
+  let parentButton = event.target.closest(".add-to-cart-btn");
+
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
+
+    addToCart(name, price);
+  }
+});
+
+function addToCart(name, price) {
+  const existingItem = cart.find((item) => item.name === name);
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
+
+  updateCartModal();
+}
+
+function updateCartModal() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item) => {
+    const cartItemContainer = document.createElement("div");
+    cartItemContainer.classList.add(
+      "flex",
+      "justify-between",
+      "mb-4",
+      "flex-col"
+    );
+
+    cartItemContainer.innerHTML = `
+            <div class=" flex items-center justify-between">
+                <div>
+                    <p class="font-medium"> ${item.name} </p>
+                    <p> Qtd: ${item.quantity} </p>
+                    <p class="font-medium mt-2"> R$: ${item.price.toFixed(
+                      2
+                    )} </p>
+                </div>
+                <div>
+                    <button class="remove-from-cart-btn" data-name="${
+                      item.name
+                    }"> Remover </button>
+                </div>
+            </div>
+        `;
+
+    total += item.price * item.quantity;
+
+    cartItemsContainer.appendChild(cartItemContainer);
+  });
+
+  cartTotal.textContent = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  cartCount.innerHTML = cart.length;
+}
