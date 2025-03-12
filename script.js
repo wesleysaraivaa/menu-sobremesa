7d01201c9cf7e182d092bf3de569a5c1570a1011
wesleysaraivaa <wesley-saraiva@outlok.com>
2025-03-11 21:51:14 -0300

    (STYLE:index.html): alterado tamanho da logo

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkOpen() {
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 18 && hora < 19;
+  return hora >= 18 && hora < 23;
 }
 
 const spanItem = document.getElementById("date-span");
